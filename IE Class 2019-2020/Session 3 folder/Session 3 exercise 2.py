775016d166db80eef88444d6acb2210f02542975
morgangeyr <morgan.geyr@gmail.com>
2019-09-11 18:19:28 +0200

    Session 3 Homework

---
+++ IE Class 2019-2020/Session 3 folder/Session 3 exercise 2.py
@@ -0,0 +1,10 @@
+import math
+print("Hello, please enter two numbers. ")
+number_1 = float(input("first number: "))
+number_2 = float(input("second number: "))
+
+number_divided = number_1/number_2
+
+print("the rounded up integer of the division is: " + str(math.ceil(number_divided)))
+
+
---
+++ IE Class 2019-2020/Session 3 folder/Session 3 exercise 3.py
@@ -0,0 +1,9 @@
+import math
+print("Hello, please enter the radius of your circle.")
+radius_1 = float(input("radius: "))
+radius_squared = radius_1**2
+
+area = radius_squared * math.pi
+
+print("The area of your circle is:_ " + str(area))
+
